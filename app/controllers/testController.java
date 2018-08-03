18424a4a4ef698cbe99afa3bb9489aa0b93407db
DonMin <maks000156@yandex.ru>
2018-08-03 22:42:31 +0400

    типо каталог есть

--- app/controllers/testController.java
+++ app/controllers/testController.java
@@ -4,17 +4,27 @@ import models.workDatabase;
 import play.mvc.Controller;
 import play.mvc.Result;
 import scala.collection.JavaConverters;
-import views.html.index;
-import views.html.indexXml;
+import views.html.indexCatalogPage;
+import views.html.indexStartPage;
 
 import java.util.ArrayList;
 
 public class testController extends Controller {
     public Result test(){
+
+        return ok(indexStartPage.render());
+    }
+    public Result catalogPage(){
         ArrayList<ArrayList<String>> stek = workDatabase.getTableValues();
 
-        return ok(indexXml.render("FUCK",
+        return ok(indexCatalogPage.render(
                 JavaConverters.asScalaBuffer(stek.get(0)).toList(),
                 JavaConverters.asScalaBuffer(stek.get(1)).toList()));
+    }
+    public Result providerPage(){
+       return ok(indexProviderPage.render());
+    }
+    public Result contactPage(){
+        return ok(indexContactPage.render());
     }
 }
