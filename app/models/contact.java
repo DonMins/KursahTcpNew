776fdb2a49906a700f8063bf8a6ce7cfc664f298
Pashina Tanya <pashinatania@mail.ru>
2018-11-05 01:09:06 +0400

    vvvv

--- app/models/contact.java
+++ app/models/contact.java
@@ -1,13 +1,50 @@
 package models;
 
-public class Contact {
-    private String contactNumber;
+import io.ebean.Ebean;
+import io.ebean.Finder;
+import play.data.validation.Constraints;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.List;
+
+@Constraints.Validate
+@Entity
+@Table(name = "contact", schema = "public")
+public class contact implements Constraints.Validatable<String>  {
+    @Id
+    @Column(name = "id_contact")
+    private Integer idContact;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "adress")
     private String adress;
+    @Column(name = "email")
     private String email;
+    @Column(name = "workHours")
     private String workHours;
 
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
+    public contact(Integer idContact, String phone, String adress, String email, String workHours)
+    {
+        this.idContact=idContact;
+        this.phone=phone;
+        this.adress=adress;
+        this.email=email;
+        this.workHours=workHours;
+    }
+    public contact(){}
+    public static Finder<Integer, contact> find = new Finder<>(contact.class);
+
+    public Integer getIdContact(){return idContact;}
+
+    public void setIdContact(Integer idContact) {
+        this.idContact = idContact;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public void setAdress(String adress) {
@@ -22,8 +59,8 @@ public class Contact {
         this.workHours = workHours;
     }
 
-    public String getContact_number() {
-        return contactNumber;
+    public String getPhone() {
+        return phone;
     }
 
     public String getAdress() {
@@ -36,6 +73,14 @@ public class Contact {
 
     public String getWorkHours() {
         return workHours;
-
+    }
+    @Override
+    public String validate() {
+        List<contact> sale = Ebean.find(contact.class).where().eq("id_contact", idContact).findList();
+        if (sale.isEmpty()) {
+            return null;
+        }
+        return "акция с таким id уже существует";
     }
 }
+
