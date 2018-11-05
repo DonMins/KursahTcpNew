7d21d45c8be1e9f424540ca6f87e156e357a7fb2
DonMin <maks000156@yandex.ru>
2018-11-05 18:55:29 +0400

    фигня кака то

--- app/controllers/ContactController.java
+++ app/controllers/ContactController.java
@@ -1,16 +1,17 @@
 package controllers;
 
 import io.ebean.Ebean;
-import models.Contact;
+
+
+import models.contact;
 import models.LoginForm;
-import models.Sale;
 import models.User;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.contactPage;
+
 
 
 import javax.inject.Inject;
@@ -22,32 +23,28 @@ import static controllers.mainPageController.getSessionLogin;
 import static scala.collection.JavaConverters.asScalaBuffer;
 
 public class ContactController extends Controller {
-    String LOGIN;
-    Boolean ADMIN;
+
     public static Integer error ;
     @Inject
     FormFactory formFactory;
     public Result contactPage()
     {
-        LOGIN = getSessionLogin();
-        ADMIN = getSessionAdmin();
-
+        String login = getSessionLogin();
+        boolean admin = getSessionAdmin();
         Form<LoginForm> form = formFactory.form(LoginForm.class);
         Form<User> form2 = formFactory.form(User.class);
-        List<Contact> contactList = Contact.find.all();
-        return ok(views.html.contactPage.render(LOGIN,ADMIN,form,form2,error,asScalaBuffer(contactList)));
+        List<contact> contactList = contact.find.all();
+        return ok(views.html.contactPage.render(login,admin,form,form2,error,asScalaBuffer(contactList)));
     }
     public Result changeContact(Integer idContact)
     {
-        LOGIN = getSessionLogin();
-        ADMIN = getSessionAdmin();
         DynamicForm dyForm = formFactory.form().bindFromRequest();
         String phone= dyForm.get("getPhone");
         String email= dyForm.get("getEmail");
         String adress= dyForm.get("getAdress");
         String workHours= dyForm.get("getWorkHours");
 
-        Contact contact = Contact.find.byId(idContact);
+        contact contact = models.contact.find.byId(idContact);
 
         contact.setAdress(adress);
         contact.setEmail(email);
